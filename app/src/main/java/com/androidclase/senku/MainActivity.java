599adbe3853d6deba6fc615a6851612833e6141b
erojasv <erojasv@cifpfbmoll.eu>
2022-03-08 19:24:24 +0100

    Initial commit

---
+++ app/src/main/java/com/androidclase/senku/MainActivity.java
@@ -0,0 +1,118 @@
+package com.androidclase.senku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mBox00;
+    private ImageView mBox01;
+    private ImageView mBox02;
+    private ImageView mBox03;
+    private ImageView mBox04;
+    private ImageView mBox05;
+    private ImageView mBox06;
+    private ImageView mBox10;
+    private ImageView mBox11;
+    private ImageView mBox12;
+    private ImageView mBox13;
+    private ImageView mBox14;
+    private ImageView mBox15;
+    private ImageView mBox16;
+    private ImageView mBox20;
+    private ImageView mBox21;
+    private ImageView mBox22;
+    private ImageView mBox23;
+    private ImageView mBox24;
+    private ImageView mBox25;
+    private ImageView mBox26;
+    private ImageView mBox30;
+    private ImageView mBox31;
+    private ImageView mBox32;
+    private ImageView mBox33;
+    private ImageView mBox34;
+    private ImageView mBox35;
+    private ImageView mBox36;
+    private ImageView mBox40;
+    private ImageView mBox41;
+    private ImageView mBox42;
+    private ImageView mBox43;
+    private ImageView mBox44;
+    private ImageView mBox45;
+    private ImageView mBox46;
+    private ImageView mBox50;
+    private ImageView mBox51;
+    private ImageView mBox52;
+    private ImageView mBox53;
+    private ImageView mBox54;
+    private ImageView mBox55;
+    private ImageView mBox56;
+    private ImageView mBox60;
+    private ImageView mBox61;
+    private ImageView mBox62;
+    private ImageView mBox63;
+    private ImageView mBox64;
+    private ImageView mBox65;
+    private ImageView mBox66;
+    private boolean selected = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBox00 = (ImageView) findViewById(R.id.box_00);
+        mBox01 = (ImageView) findViewById(R.id.box_01);
+        mBox02 = (ImageView) findViewById(R.id.box_02);
+        mBox03 = (ImageView) findViewById(R.id.box_03);
+        mBox04 = (ImageView) findViewById(R.id.box_04);
+        mBox05 = (ImageView) findViewById(R.id.box_05);
+        mBox06 = (ImageView) findViewById(R.id.box_06);
+        mBox10 = (ImageView) findViewById(R.id.box_10);
+        mBox11 = (ImageView) findViewById(R.id.box_11);
+        mBox12 = (ImageView) findViewById(R.id.box_12);
+        mBox13 = (ImageView) findViewById(R.id.box_13);
+        mBox14 = (ImageView) findViewById(R.id.box_14);
+        mBox15 = (ImageView) findViewById(R.id.box_15);
+        mBox16 = (ImageView) findViewById(R.id.box_16);
+        mBox20 = (ImageView) findViewById(R.id.box_20);
+        mBox21 = (ImageView) findViewById(R.id.box_21);
+        mBox22 = (ImageView) findViewById(R.id.box_22);
+        mBox23 = (ImageView) findViewById(R.id.box_23);
+        mBox24 = (ImageView) findViewById(R.id.box_24);
+        mBox25 = (ImageView) findViewById(R.id.box_25);
+        mBox26 = (ImageView) findViewById(R.id.box_26);
+        mBox30 = (ImageView) findViewById(R.id.box_30);
+        mBox31 = (ImageView) findViewById(R.id.box_31);
+        mBox32 = (ImageView) findViewById(R.id.box_32);
+        mBox33 = (ImageView) findViewById(R.id.box_33);
+        mBox34 = (ImageView) findViewById(R.id.box_34);
+        mBox35 = (ImageView) findViewById(R.id.box_35);
+        mBox36 = (ImageView) findViewById(R.id.box_36);
+        mBox40 = (ImageView) findViewById(R.id.box_40);
+        mBox41 = (ImageView) findViewById(R.id.box_41);
+        mBox42 = (ImageView) findViewById(R.id.box_42);
+        mBox43 = (ImageView) findViewById(R.id.box_43);
+        mBox44 = (ImageView) findViewById(R.id.box_44);
+        mBox45 = (ImageView) findViewById(R.id.box_45);
+        mBox46 = (ImageView) findViewById(R.id.box_46);
+        mBox50 = (ImageView) findViewById(R.id.box_50);
+        mBox51 = (ImageView) findViewById(R.id.box_51);
+        mBox52 = (ImageView) findViewById(R.id.box_52);
+        mBox53 = (ImageView) findViewById(R.id.box_53);
+        mBox54 = (ImageView) findViewById(R.id.box_54);
+        mBox55 = (ImageView) findViewById(R.id.box_55);
+        mBox56 = (ImageView) findViewById(R.id.box_56);
+        mBox60 = (ImageView) findViewById(R.id.box_60);
+        mBox61 = (ImageView) findViewById(R.id.box_61);
+        mBox62 = (ImageView) findViewById(R.id.box_62);
+        mBox63 = (ImageView) findViewById(R.id.box_63);
+        mBox64 = (ImageView) findViewById(R.id.box_64);
+        mBox65 = (ImageView) findViewById(R.id.box_65);
+        mBox66 = (ImageView) findViewById(R.id.box_66);
+
+    }
+}
